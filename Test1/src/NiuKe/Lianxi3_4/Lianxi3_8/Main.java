685952fc23cbe435010dd3034614ba20d78a32c3
qingmou <Lidanzing@163.com>
2019-03-08 17:56:04 +0800

    练习

---
+++ Test1/src/NiuKe/Lianxi3_4/Lianxi3_8/Main.java
@@ -0,0 +1,48 @@
+package NiuKe.Lianxi3_4.Lianxi3_8;
+
+import java.util.Scanner;
+
+/**
+ * Author:lidan
+ * Created:2019/3/8
+ */
+public class Main {
+    public int buyApple(int n) {
+        if (n < 6 || n % 2 != 0 || n == 10) {
+            return -1;
+        }
+        int i = n / 8;
+        int j = n % 8;
+        if (j == 0) {
+            return i;
+        } else {
+            return 1 + n / 8;
+        }
+        }
+
+    public static void main(String[] args) {
+        Main main = new Main();
+        Scanner in=new Scanner(System.in);
+        int n=in.nextInt();
+        int k = main.buyApple(n);
+        System.out.println(k);
+    }
+}
+
+//public class Main {
+//    public static void main(String[] args) {
+//        int n=20;
+//        int min=n;
+//        if(n%2==1) System.out.println("-1");
+//        else{
+//            for(int i=1;i<n/6;i++){
+//                if((n-6*i)%8==0){
+//                    int j=(n-6*i)/8;
+//                    min=min>(i+j)?(i+j):min;
+//                }
+//            }
+//        }
+//
+//        System.out.println(min);
+//    }
+//}
