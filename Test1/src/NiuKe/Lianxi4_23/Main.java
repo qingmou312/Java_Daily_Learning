69935569b16056d5fe901707738f4a6034931192
qingmou <Lidanzing@163.com>
2019-04-24 21:12:47 +0800

    日常练习

--- Test1/src/NiuKe/Lianxi4_23/Main.java
+++ Test1/src/NiuKe/Lianxi4_23/Main.java
@@ -2,22 +2,20 @@ package NiuKe.Lianxi4_23;
 
 import java.util.Scanner;
 
-/**
- * Author:lidan
- * Created:2019/4/23
- */
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        String str1 = in.nextLine();
-        if (str1.length() > 100) {
-            System.out.println("请重新输入：");
-        } else {
+        while (in.hasNextLine()) {
+            String str1 = in.nextLine();
             String[] str2 = str1.split("_");
             StringBuffer sb = new StringBuffer();
-            for (int i = 0; i < str2.length-1; i++) {
-                sb.append(0);
-                sb.append(str2[i+1].substring(0, 1).toUpperCase() + str2[i+1].substring(1));
+            sb.append(str2[0]);
+            for (int i = 1; i < str2.length; i++) {
+                if (str2[i].toCharArray().length == 1) {
+                    sb.append(str2[i].toUpperCase());
+                } else {
+                    sb.append(str2[i].substring(0, 1).toUpperCase() + str2[i].substring(1));
+                }
             }
             System.out.println(sb.toString());
         }
--- Test1/src/NiuKe/Lianxi4_23/Main2.java
+++ Test1/src/NiuKe/Lianxi4_23/Main2.java
@@ -7,15 +7,36 @@ import java.util.Scanner;
  * Created:2019/4/23
  */
 public class Main2 {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        String str1 = in.nextLine();
-        String[] strings = str1.split(" ");
-        StringBuffer sb = new StringBuffer();
-        for (int i = 0; i < strings.length; i++) {
-            sb.append(strings[strings.length - 1 - i]);
-            sb.append(" ");
+    //    public static void main(String[] args) {
+//        Scanner in = new Scanner(System.in);
+//        while (in.hasNextLine()) {
+//            String str1 = in.nextLine();
+//            String[] strings = str1.split("[^a-zA-Z]");
+//            StringBuffer sb = new StringBuffer();
+//            for (int i = 0; i < strings.length; i++) {
+//                sb.append(strings[strings.length - 1 - i]);
+//                if (strings.length - 1 - i != 0) {
+//                    sb.append(" ");
+//                }
+//            }
+//            System.out.println(sb.toString());
+//        }
+//    }
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()) {
+            String str = sc.nextLine();
+            // 使用正则表达式作为分割符.
+            String[] strArray = str.split("[^a-zA-Z]+");
+            for (int i = strArray.length - 1; i >= 2; i--) {
+                System.out.print(strArray[i] + ' ');
+            }
+            if (strArray[0].length() == 0)//如果字符串数组的第一个元素是空串，那么下标为1的元素就是最后一
+                System.out.println(strArray[1]);
+            else
+                System.out.println(strArray[1] + ' ' + strArray[0]);
         }
-        System.out.println(sb.toString());
     }
+
+
 }
