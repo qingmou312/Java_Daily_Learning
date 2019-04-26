cd6cbd67ef226cd75e364dca78728adadb13a58e
qingmou <Lidanzing@163.com>
2019-04-26 21:39:34 +0800

    日常练习

--- Test1/src/Java5/Heap/Heap.java
+++ Test1/src/Java5/Heap/Heap.java
@@ -5,7 +5,6 @@ package Java5.Heap;
  * Created:2019/4/25
  */
 public class Heap {
-
     /**
      * 向下调整
      * 必须满足可以向下调整的前提：只有一个位置不满足堆
@@ -27,7 +26,6 @@ public class Heap {
         /**
          * 不是叶子节点，意味着一定有左孩子，但不一定有右孩子
          * 2.找到最大的一个孩子
-         *
          */
         int right = 2 * index + 2;
         int max = left;
@@ -45,6 +43,29 @@ public class Heap {
             tree[max] = tmp;
         }
         heapif(tree, max);
+    }
+
+    /**
+     * 向上调整
+     * @param array
+     * @param size
+     * @param index
+     */
+
+    public void adjustUp(int[] array, int size, int index) {
+        while (index > 0) {
+            int parent = (index - 1) / 2;
+            if (parent < 0) {
+                return;
+            }
+            if (array[parent] >= array[index]) {
+                break;
+            }
+            int tmp = array[parent];
+            array[parent] = array[index];
+            array[index] = tmp;
+            index = parent;
+        }
     }
 
     public static void main(String[] args) {
