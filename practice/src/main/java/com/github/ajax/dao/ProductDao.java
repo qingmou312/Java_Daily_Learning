63ac4037fd85bf3d2366f2c4aab83b679f53f359
qingmou <Lidanzing@163.com>
2019-05-21 19:39:55 +0800

    JavaWeb练习

---
+++ practice/src/main/java/com/github/ajax/dao/ProductDao.java
@@ -0,0 +1,9 @@
+package com.github.ajax.dao;
+
+/**
+ * Author:lidan
+ * Created:2019/5/21
+ */
+public class ProductDao {
+}
+
---
+++ practice/src/main/java/com/github/ajax/service/ProductService.java
@@ -0,0 +1,9 @@
+package com.github.ajax.service;
+
+/**
+ * Author:lidan
+ * Created:2019/5/21
+ */
+public class ProductService {
+
+}
