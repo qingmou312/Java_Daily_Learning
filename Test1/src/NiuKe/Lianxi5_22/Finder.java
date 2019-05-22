7b66c16d55b1c28f04c3865dd496467f95f9beee
qingmou <Lidanzing@163.com>
2019-05-22 21:58:50 +0800

    日常练习

---
+++ Test1/src/NiuKe/Lianxi5_22/Finder.java
@@ -0,0 +1,32 @@
+package NiuKe.Lianxi5_22;
+
+/**
+ * Author:lidan
+ * Created:2019/5/22
+ */
+
+import java.util.*;
+
+public class Finder {
+    public int findKth(int[] a, int n, int K) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n - 1 - i; j++) {
+                if (a[j] > a[j + 1]) {
+                    int tmp = a[j];
+                    a[j] = a[j + 1];
+                    a[j + 1] = tmp;
+                }
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            System.out.println(a[i]);
+        }
+        return a[K - 1];
+    }
+
+    public static void main(String[] args) {
+        int[] a = {1, 3, 5, 2, 2};
+        Finder finder = new Finder();
+        System.out.println(finder.findKth(a, 5, 3));
+    }
+}
